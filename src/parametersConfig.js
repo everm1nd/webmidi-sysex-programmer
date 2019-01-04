d8508f86772455bdf5b1d27b998e2f79ff9696db
Nikita <everm1nd.mail@gmail.com>
2019-01-04 19:09:46 +0100

    added more parameters in parameters list

--- src/parametersConfig.js
+++ src/parametersConfig.js
@@ -40,5 +40,42 @@ export default [
     name: 'OP4 - Rate Scaling',
     min: 0,
     max: 3
+  },
+  {
+    id: 7,
+    name: 'OP4 - EG Bias Sensitivity',
+    min: 0,
+    max: 7
+  },
+  {
+    id: 8,
+    name: 'OP4 - Amplitude Modulation Enable',
+    min: 0,
+    max: 1
+  },
+  {
+    id: 9,
+    name: 'OP4 - Key Velocity Sensitivity',
+    min: 0,
+    max: 7
+  },
+  {
+    id: 10,
+    name: 'OP4 - Operator Output Level',
+    min: 0,
+    max: 99
+  },
+  {
+    id: 11,
+    name: 'OP4 - Frequency',
+    min: 0,
+    max: 63
+  },
+  {
+    id: 12,
+    name: 'OP4 - Detune',
+    min: 0,
+    max: 6,
+    default: 3
   }
 ]
