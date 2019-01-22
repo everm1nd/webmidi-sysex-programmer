b0a09124155f3c8102216f166b2b2f5d9d9e75a4
Nikita <everm1nd.mail@gmail.com>
2019-01-22 17:58:27 +0100

    added function to render operator parameters and render parameters of 4 operators in the list

--- src/parametersConfig.js
+++ src/parametersConfig.js
@@ -1,81 +1,92 @@
+const operatorParameters = (operatorNumber, offset) => {
+  const offsetId = (param) => Object.assign({}, param, { id: param.id + offset })
+
+  return [
+    {
+      id: 0,
+      name: `OP${operatorNumber} - Attack Rate`,
+      min: 0,
+      max: 31
+    },
+    {
+      id: 1,
+      name: `OP${operatorNumber} - Decay 1 Rate`,
+      min: 0,
+      max: 31
+    },
+    {
+      id: 2,
+      name: `OP${operatorNumber} - Decay 2 Rate`,
+      min: 0,
+      max: 31
+    },
+    {
+      id: 3,
+      name: `OP${operatorNumber} - Release Rate`,
+      min: 1,
+      max: 15
+    },
+    {
+      id: 4,
+      name: `OP${operatorNumber} - Decay 1 Level`,
+      min: 0,
+      max: 15
+    },
+    {
+      id: 5,
+      name: `OP${operatorNumber} - Level Scaling`,
+      min: 0,
+      max: 99
+    },
+    {
+      id: 6,
+      name: `OP${operatorNumber} - Rate Scaling`,
+      min: 0,
+      max: 3
+    },
+    {
+      id: 7,
+      name: `OP${operatorNumber} - EG Bias Sensitivity`,
+      min: 0,
+      max: 7
+    },
+    {
+      id: 8,
+      name: `OP${operatorNumber} - Amplitude Modulation Enable`,
+      min: 0,
+      max: 1
+    },
+    {
+      id: 9,
+      name: `OP${operatorNumber} - Key Velocity Sensitivity`,
+      min: 0,
+      max: 7
+    },
+    {
+      id: 10,
+      name: `OP${operatorNumber} - Operator Output Level`,
+      min: 0,
+      max: 99
+    },
+    {
+      id: 11,
+      name: `OP${operatorNumber} - Frequency`,
+      min: 0,
+      max: 63
+    },
+    {
+      id: 12,
+      name: `OP${operatorNumber} - Detune`,
+      min: 0,
+      max: 6,
+      default: 3
+    }
+  ].map(offsetId)
+}
+
 export default [
-  {
-    id: 0,
-    name: 'OP4 - Attack Rate',
-    min: 0,
-    max: 31
-  },
-  {
-    id: 1,
-    name: 'OP4 - Decay 1 Rate',
-    min: 0,
-    max: 31
-  },
-  {
-    id: 2,
-    name: 'OP4 - Decay 2 Rate',
-    min: 0,
-    max: 31
-  },
-  {
-    id: 3,
-    name: 'OP4 - Release Rate',
-    min: 1,
-    max: 15
-  },
-  {
-    id: 4,
-    name: 'OP4 - Decay 1 Level',
-    min: 0,
-    max: 15
-  },
-  {
-    id: 5,
-    name: 'OP4 - Level Scaling',
-    min: 0,
-    max: 99
-  },
-  {
-    id: 6,
-    name: 'OP4 - Rate Scaling',
-    min: 0,
-    max: 3
-  },
-  {
-    id: 7,
-    name: 'OP4 - EG Bias Sensitivity',
-    min: 0,
-    max: 7
-  },
-  {
-    id: 8,
-    name: 'OP4 - Amplitude Modulation Enable',
-    min: 0,
-    max: 1
-  },
-  {
-    id: 9,
-    name: 'OP4 - Key Velocity Sensitivity',
-    min: 0,
-    max: 7
-  },
-  {
-    id: 10,
-    name: 'OP4 - Operator Output Level',
-    min: 0,
-    max: 99
-  },
-  {
-    id: 11,
-    name: 'OP4 - Frequency',
-    min: 0,
-    max: 63
-  },
-  {
-    id: 12,
-    name: 'OP4 - Detune',
-    min: 0,
-    max: 6,
-    default: 3
-  }
+  ...operatorParameters(4,0),
+  ...operatorParameters(3,13),
+  ...operatorParameters(2,26),
+  ...operatorParameters(1,39)
 ]
