6365bb09088fa13471c5a2f7ef2376a2def8f7f2
Nikita <everm1nd.mail@gmail.com>
2019-02-07 18:36:04 +0100

    WIP: RXJS

---
+++ src/midiBridge.js
@@ -0,0 +1,8 @@
+import { fromEvent } from "rxjs";
+import { throttleTime, map, groupBy, mergeMap } from "rxjs/operators";
+
+const midiBridge = (midiOutput) => {
+  return {}
+}
+
+export default midiBridge;
