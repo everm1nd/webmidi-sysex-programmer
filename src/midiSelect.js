14c9706346589dd5854cd10617087b21ffbf5c49
Nikita <everm1nd.mail@gmail.com>
2019-02-28 12:31:08 +0100

    simplified code of MidiSelect component

--- src/midiSelect.js
+++ src/midiSelect.js
@@ -2,33 +2,19 @@ import React from "react";
 import WebMidi from "webmidi";
 
 class MidiSelect extends React.Component {
-  state = {
-    midiOutputs: [],
-    midiOutputId: 0 // select first interface from the list
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      midiOutputs: nextProps.active ? WebMidi.outputs : []
-    })
-  }
-
-  _changeOutput(event) {
-    const interfaceId = event.target.value;
-    this.setState({
-      midiOutputId: interfaceId
-    }, () => this.props.onChange(WebMidi.outputs[this.state.midiOutputId]))
+  _changeOutput({ target: { value: interfaceId } }) {
+    this.props.onChange(WebMidi.outputs[interfaceId])
   }
 
   _midiOutputs() {
-    return this.state.midiOutputs.map(({ name }, index) => (
+    return WebMidi.outputs.map(({ name }, index) => (
       <option key={name} value={index}>{name}</option>
     ));
   }
 
   render() {
     return (
-      <select name="output" onChange={this._changeOutput.bind(this)} disabled={!this.props.active}> // value={this.state.midiOutputId}
+      <select name="output" onChange={this._changeOutput.bind(this)} disabled={!this.props.active}>
         <option value="">select MIDI output</option>
         {this._midiOutputs()}
       </select>
