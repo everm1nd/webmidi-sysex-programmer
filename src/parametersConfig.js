1177b675690784e8a15602fe6574751ab153e7da
Nikita <everm1nd.mail@gmail.com>
2019-04-04 16:17:53 +0200

    added `type` field to parameter in parametersConfig

--- src/parametersConfig.js
+++ src/parametersConfig.js
@@ -1,81 +1,102 @@
+// Parameter types
+// VCED - Voice Edit Parameters
+// ACED - Voice Edit Additional Parameters
+const TYPES = {
+  VCED: 'VCED',
+  ACED: 'ACED'
+}
+
 const operatorParameters = (operatorNumber, offset) => {
   const offsetId = (param) => Object.assign({}, param, { id: param.id + offset })
 
   return [
     {
       id: 0,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Attack Rate`,
       min: 0,
       max: 31
     },
     {
       id: 1,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Decay 1 Rate`,
       min: 0,
       max: 31
     },
     {
       id: 2,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Decay 2 Rate`,
       min: 0,
       max: 31
     },
     {
       id: 3,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Release Rate`,
       min: 1,
       max: 15
     },
     {
       id: 4,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Decay 1 Level`,
       min: 0,
       max: 15
     },
     {
       id: 5,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Level Scaling`,
       min: 0,
       max: 99
     },
     {
       id: 6,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Rate Scaling`,
       min: 0,
       max: 3
     },
     {
       id: 7,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - EG Bias Sensitivity`,
       min: 0,
       max: 7
     },
     {
       id: 8,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Amplitude Modulation Enable`,
       min: 0,
       max: 1
     },
     {
       id: 9,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Key Velocity Sensitivity`,
       min: 0,
       max: 7
     },
     {
       id: 10,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Operator Output Level`,
       min: 0,
       max: 99
     },
     {
       id: 11,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Frequency`,
       min: 0,
       max: 63
     },
     {
       id: 12,
+      type: TYPES.VCED,
       name: `OP${operatorNumber} - Detune`,
       min: 0,
       max: 6,
@@ -91,66 +112,77 @@ export default [
   ...operatorParameters(1,39),
   {
     id: 52,
+    type: TYPES.VCED,
     name: 'Algorithm',
     min: 0,
     max: 7
   },
   {
     id: 53,
+    type: TYPES.VCED,
     name: 'Feedback',
     min: 0,
     max: 7
   },
   {
     id: 54,
+    type: TYPES.VCED,
     name: 'LFO Speed',
     min: 0,
     max: 99
   },
   {
     id: 55,
+    type: TYPES.VCED,
     name: 'LFO Delay',
     min: 0,
     max: 99
   },
   {
     id: 56,
+    type: TYPES.VCED,
     name: 'Pitch Modulation Depth',
     min: 0,
     max: 99
   },
   {
     id: 57,
+    type: TYPES.VCED,
     name: 'Amplitude Modulation Depth',
     min: 0,
     max: 99
   },
   {
     id: 58,
+    type: TYPES.VCED,
     name: 'LFO Sync',
     min: 0,
     max: 1
   },
   {
     id: 59,
+    type: TYPES.VCED,
     name: 'LFO Wave',
     min: 0,
     max: 3
   },
   {
     id: 60,
+    type: TYPES.VCED,
     name: 'Pitch Modulation Sensitivity',
     min: 0,
     max: 7
   },
   {
     id: 61,
+    type: TYPES.VCED,
     name: 'Amplitude Modulation Sensitivity',
     min: 0,
     max: 3
   },
   {
     id: 62,
+    type: TYPES.VCED,
     name: 'Transpose',
     min: 0,
     max: 48,
@@ -158,78 +190,91 @@ export default [
   },
   {
     id: 63,
+    type: TYPES.VCED,
     name: 'Poly/Mono',
     min: 0,
     max: 1
   },
   {
     id: 64,
+    type: TYPES.VCED,
     name: 'Pitch Bend Range',
     min: 0,
     max: 12
   },
   {
     id: 65,
+    type: TYPES.VCED,
     name: 'Portamento Mode',
     min: 0,
     max: 1
   },
   {
     id: 66,
+    type: TYPES.VCED,
     name: 'Portamento Time',
     min: 0,
     max: 99
   },
   {
     id: 67,
+    type: TYPES.VCED,
     name: 'Foot Control Volume',
     min: 0,
     max: 99
   },
   {
     id: 68,
+    type: TYPES.VCED,
     name: 'Sustain',
     min: 0,
     max: 99
   },
   {
     id: 69,
+    type: TYPES.VCED,
     name: 'Portamento',
     min: 0,
     max: 1
   },
   {
     id: 70,
+    type: TYPES.VCED,
     name: 'Chorus',
     min: 0,
     max: 1
   },
   {
     id: 71,
+    type: TYPES.VCED,
     name: 'Modulation Wheel Pitch',
     min: 0,
     max: 99
   },
   {
     id: 72,
+    type: TYPES.VCED,
     name: 'Modulation Wheel Amplitude',
     min: 0,
     max: 99
   },
   {
     id: 73,
+    type: TYPES.VCED,
     name: 'Breath Control Pitch',
     min: 0,
     max: 99
   },
   {
     id: 74,
+    type: TYPES.VCED,
     name: 'Breath Control Amplitude',
     min: 0,
     max: 99
   },
   {
     id: 75,
+    type: TYPES.VCED,
     name: 'Breath Control Pitch Bias',
     min: 0,
     max: 99,
@@ -237,6 +282,7 @@ export default [
   },
   {
     id: 76,
+    type: TYPES.VCED,
     name: 'Breath Control EG Bias',
     min: 0,
     max: 99
@@ -244,60 +290,70 @@ export default [
   // TODO: replace this by-character input with normal string
   {
     id: 77,
+    type: TYPES.VCED,
     name: 'Voice name char 1',
     min: 32,
     max: 127
   },
   {
     id: 78,
+    type: TYPES.VCED,
     name: 'Voice name char 2',
     min: 32,
     max: 127
   },
   {
     id: 79,
+    type: TYPES.VCED,
     name: 'Voice name char 3',
     min: 32,
     max: 127
   },
   {
     id: 80,
+    type: TYPES.VCED,
     name: 'Voice name char 4',
     min: 32,
     max: 127
   },
   {
     id: 81,
+    type: TYPES.VCED,
     name: 'Voice name char 5',
     min: 32,
     max: 127
   },
   {
     id: 82,
+    type: TYPES.VCED,
     name: 'Voice name char 6',
     min: 32,
     max: 127
   },
   {
     id: 83,
+    type: TYPES.VCED,
     name: 'Voice name char 7',
     min: 32,
     max: 127
   },
   {
     id: 84,
+    type: TYPES.VCED,
     name: 'Voice name char 8',
     min: 32,
     max: 127
   },
   {
     id: 85,
+    type: TYPES.VCED,
     name: 'Voice name char 9',
     min: 32,
     max: 127
   },
   {
     id: 86,
+    type: TYPES.VCED,
     name: 'Voice name char 10',
     min: 32,
     max: 127
@@ -307,6 +363,7 @@ export default [
   // TODO: find some nice interface solution
   {
     id: 93,
+    type: TYPES.VCED,
     name: 'Operator 4-1 On/Off',
     min: 0,
     max: 15
